c9df8c44d9e08538eb9af67a600b57df20aab0d4
thiago <tmariguella@gmail.com>
2020-10-21 18:04:39 -0300

    projetos php

---
+++ projeto/mvc/Views/pages/home.php
@@ -0,0 +1,49 @@
+<div class="chamada">
+	<div class="center">
+	<h2><?php echo $arr['titulo'] ?></h2>
+	</div><!--center-->
+</div><!--chamada-->
+<div class="home principal">
+	<div class="center">
+		<h2>Desenvolvendo com seriadade e proficionalismo</h2>
+		<div class="alerta">
+			<h3>Experiencias com:</h3>
+		</div><!--alerta-->
+		<div class="exper center">
+			<table>
+				<h3 class="alerta">Front</h3>
+				<tr>
+					<th>Linguagem:</th>
+					<th>HTML</th>
+					<th>CSS</th>
+					<th>JS</th>
+				</tr>
+				<tr>
+					<th>Framework:</th>
+					<th>Angolar,</th>
+					<th>React,</th>
+					<th>Vue,</th>
+					<th>Jquery.</th>
+				</tr>
+			</table>
+			</div><!--exper-->
+			<div class="exper center ">
+				<table>
+				<h3 class="alerta">back</h3>
+				
+				<tr>
+					<th>Linguagem:</th>
+					<th>PHP</th>
+					<th>SQL</th>
+				</tr>
+				<tr>
+					<th>Framework:</th>
+					<th>Laravel,</th>
+					<th>Zend,</th>
+					<th>Slim,</th>
+					<th>CodeIgniter.</th>
+				</tr>
+			</table>
+			</div><!--exper-->
+	</div>
+</div><!--sobre-->
---
+++ projeto/mvc/Views/pages/sobre.php
@@ -0,0 +1,12 @@
+<div class="chamada">
+	<div class="center">
+	<h2><?php echo $arr['titulo'] ?></h2>
+	</div><!--center-->
+</div><!--chamada-->
+<div class="sobre principal">
+	<div class="center">
+		<h2>Thiago Mariguella Filomeno</h2>
+		<h6>Uma pessoa simple, que esta sempre procurando novos conhecimentos.</h6>
+		<p>Com todo esse conhecimento adiquirido, hoje e pocivel veu o quanto evoluiu com o passar do tempo.</p>
+	</div>
+</div><!--sobre-->
